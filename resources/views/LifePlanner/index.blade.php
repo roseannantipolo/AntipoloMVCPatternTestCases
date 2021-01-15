b70827b9dbf9dcc618e54c1ff8957dfe1c13fbef
Rose Ann <57524103+roseannantipolo@users.noreply.github.com>
2021-01-15 23:05:49 +0800

    my first commit

---
+++ resources/views/LifePlanner/index.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> Antipolo - Life Planner </title>
+        
+        <link href="/css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <header class="intro">
+        <div class="introduction">
+            <h1>LIFE PLANNER</h1>
+            <h3>Get Organized!</h3>
+        </div>
+        </header>
+    </body>
+</html>
