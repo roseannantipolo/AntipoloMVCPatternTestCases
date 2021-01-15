0b5812adf58e265d81fd3a91f678a74d94e327c4
Rose Ann <57524103+roseannantipolo@users.noreply.github.com>
2021-01-16 00:09:58 +0800

    second commit - Added the content of my project

--- resources/views/LifePlanner/index.blade.php
+++ resources/views/LifePlanner/index.blade.php
@@ -11,6 +11,47 @@
             <h1>LIFE PLANNER</h1>
             <h3>Get Organized!</h3>
         </div>
+
+        <div class="storage">
+            <h2>To-Do-List</h2>
+            <form action="addTask.php" method="POST">
+            <input type="text" class="addTodo" name="addTodo" placeholder="Add your Tasks" />
+            <button class="todobutton" name="taskBtn">
+                <img src="add btn.png" />
+            </button>
+            </form>
+        </div>
+
+        <div class="container">
+            <h2>Goals and Plans</h2>
+            <form action="addTask.php" method="POST">
+            <input type="text" name="addGoalsPlans" class="addGoalsPlans" placeholder="Add your Goals and Plans" />
+            <button class="goalsPlansButton" name="goalsBtn">
+                <img src="add btn.png" />
+            </button>
+            </form>
+        </div>
+
+        <div class="holder">
+            <h2>Appointments</h2>
+            <form action="addTask.php" method="POST">
+            <input type="text" name="addAppointments" class="addAppointments" placeholder="Add your Appointments" />
+            <button class="appointmentsButton" name="appointmentsBtn">
+                <img src="add btn.png" />
+            </button>
+            </form>
+        </div>
+
+        <div class="repo">
+            <h2>Errands</h2>
+            <form action="addTask.php" method="POST">
+            <input type="text" name="addErrands" class="addErrands" placeholder="Add your Errands" />
+            <button class="errandsButton" name="errandsBtn">
+                <img src="add btn.png" />
+            </button>
+            </form>
+        </div>
+        
         </header>
     </body>
 </html>
